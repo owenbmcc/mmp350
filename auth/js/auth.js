d7664b92fbf66cdf97cab3f4ef0917332a10426d
Owen Roberts <oroberts@bmcc.cuny.edu>
2020-02-05 21:04:07 -0500

    finished login

--- auth/js/auth.js
+++ auth/js/auth.js
@@ -7,6 +7,7 @@ const loginEmail = document.getElementById("login-email");
 const loginPassword = js.getEl("login-password");
 const loginButton = js.getEl("login-button");
 const loginMessage = js.getEl("login-message");
+const userName = js.getEl("user-name");
 
 /*
 	event listener 
@@ -23,3 +24,6 @@ function onError(errorMessage) {
 	loginMessage.textContent = errorMessage;
 }
 
+function userLoggedIn(uid, displayName) {
+	userName.textContent = "Welcome " + displayName;
+}
